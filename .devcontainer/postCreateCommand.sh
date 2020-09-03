a3ce25e305937459aa5275c65e2964ed5d377d8d
rafoid <rafoid@gmail.com>
2020-09-02 22:24:28 -0700

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-sudo chown vscode -R $(pwd)
-
-./configure; make; sudo make install
 
 echo "Appending github.ibm.com to ~/.ssh/known_hosts"
 mkdir ~/.ssh && /usr/bin/ssh-keyscan -H github.com >> ~/.ssh/known_hosts
